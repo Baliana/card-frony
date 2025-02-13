eb26ab451460a84b6b8e66206a49dc13e7d93807
FERNANDO BALIANA DE MIRANDA <24122781@CFP127ALUNO.LOCAL>
2025-02-13 12:26:23 -0300

    terminar app.js

--- app.js
+++ app.js
@@ -29,20 +29,64 @@ const produtos = [{
         nome: 'Iphone 16 Pro Max',
         icon: 'iphone.jpg',
         descricao: 'Apple iPhone 16 Pro Max (256 GB) Titânio-deserto',
+        preco: 'R$ 10.999.00'
     },
     {
         nome: 'Samsung Galaxy S24 Ultra',
-        icon: '',
-        descricao: ''
+        icon: 'S24',
+        descricao: 'Smartphone Samsung Galaxy S23 Ultra 512GB Verde 5G 12GB RAM 6,8” Câm. Quádrupla + Selfie 12MP',
+        preco: 'R$ 6.499,00'
     },
     {
         nome: 'Xiaomi 14',
-        icon: '',
-        descricao: ''
+        icon: 'Xiaomi',
+        descricao: 'Smartphone Xiaomi Redmi Note 14 256GB 8GB RAM Dual SIM Tela 6.67" - Preto',
+        preco: 'R$ 1.557,59'
+    },
+    {
+        nome: 'Nokia',
+        icon: 'nokia',
+        descricao: 'lanterna Original Nokia 1100 Gsm900',
+        preco: '81,59'
+
     }
 
 ]
 
-function criarBanner() {
-    const novaImg = document.createElement('img')
+function criarBanner(Celulares) {
+    const container = document.getElementById('container')
+    container.className = 'container'
+
+    const card = document.createElement('div')
+    card.className = 'card' 
+
+    const Titulo = document.createElement('div')
+    Titulo.className = 'Titulo' 
+    
+    const Titulop = document.createElement('p')
+   
+
+    const image = document.createElement('div')
+    image.className = 'image'
+
+    const img = document.createElement('img')
+
+     const comentario = document.createElement('div')
+     comentario.className = 'comentario'
+
+     const comentarioh1 = document.createElement('h1')
+
+     const comentariop = document.createElement('p')
+
+     const preco = document.createElement('p')
+
+     const listaBotao = document.createElement('div')
+     listaBotao.className = 'listaBotao'
+
+     const Modelos = document.createElement('p')
+
+     const compra = document.createElement('div')
+     compra.className = 'compra'
+
+
 }
